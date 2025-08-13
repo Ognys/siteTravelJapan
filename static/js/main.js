bbfdd6aaa4be6f8db57902ee6d86ff19ebb7a3b2
Ogny <Myth4321@mail.ru>
2025-08-13 06:27:09 -0700

    Закончен адаптив под разрешения планшетов? начат под мобльные устройства

--- static/js/main.js
+++ static/js/main.js
@@ -9,3 +9,18 @@
         }
     };
 }());
+
+// Burger
+
+(function () {
+    const burger = document.querySelector(".burger");
+    const menu = document.querySelector(".header-nav");
+    const close = document.querySelector(".header-nav-menu-close");
+    burger.addEventListener("click", () => {
+        menu.classList.add("header-nav-active");
+    });
+        close.addEventListener("click", () => {
+        menu.classList.remove("header-nav-active");
+    });
+
+}());
